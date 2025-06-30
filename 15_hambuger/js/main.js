4af338c37cb529159e684c213f1ea673816b8ef2
kimjunhoe <wns5254@naver.com>
2025-06-30 15:32:29 +0900

    Add files via upload

---
+++ 15_hambuger/js/main.js
@@ -0,0 +1,5 @@
+$(function(){
+    $(".box a").click(function(){
+        $(this).toggleClass("on")
+    })
+})
---
+++ 15_hambuger/js/main2.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+
+// }) 이렇게 쓸 수 있음
+$(function(){
+    $(".box a").click(function(){
+        $(".box a").toggleClass("on")
+    })
+})
