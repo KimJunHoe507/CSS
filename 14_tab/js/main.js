06f38bbe2780c31746adb9b43cf02bd63878684d
kimjunhoe <wns5254@naver.com>
2025-07-01 16:44:41 +0900

    Add files via upload

--- 14_tab/js/main.js
+++ 14_tab/js/main.js
@@ -2,5 +2,10 @@ $(function(){
     $(".menu li").click(function(){
         $(".menu li").removeClass("on")
         $(this).addClass("on");
+        
+        let num=$(this).index();
+
+        $(".list").hide();
+        $(".list").eq(num).show();
     })
 })
