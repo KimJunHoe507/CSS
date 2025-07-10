eb691cf912ff0f8bdcc61facd01e68b49c95e01a
kimjunhoe <wns5254@naver.com>
2025-07-10 17:11:36 +0900

    Add files via upload

---
+++ 08_subway/js/main.js
@@ -0,0 +1,35 @@
+$(function(){
+    $('.a_slide').slick({
+        // 자동실행
+        autoplay: true,
+        // 한번에 보여줄 개수
+        slidesToShow: 1,
+        // 좌우 버튼 숨기기
+        arrows: false
+    });
+    $('.b_slide').slick({
+        // 자동실행
+        autoplay: true,
+        // 한번에 보여줄 개수
+        slidesToShow: 2,
+        // 좌우 버튼 숨기기
+        arrows: false,
+
+        dots:true,
+    });
+
+    $('.m_gnb>ul>li>a').click(function(){
+        $(this).next().slideToggle().parent().siblings().find('.depth2').slideUp();
+    });
+
+    $('.all_menu').click(function(){
+        $('.cover').fadeIn();
+        $('.m_side_wrap').animate({'left':0},300);
+    });
+
+    $('.m_close').click(function(){
+        $('.m_side_wrap').animate({'left':'-100%'},300);
+        $('.cover').fadeOut();
+    });
+
+})
