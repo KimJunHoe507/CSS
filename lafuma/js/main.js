5e3bd54b3b8f3de94ad07efe6b2c6bcc10953100
kimjunhoe <wns5254@naver.com>
2025-07-16 11:44:19 +0900

    Add files via upload

--- lafuma/js/main.js
+++ lafuma/js/main.js
@@ -26,3 +26,26 @@ $(function () {
 
     });
 });
+
+$(function(){
+    $('.main__content--list li').mouseenter(function(){
+        // attr : 속성 가져오기
+        let num = $(this).find('img').attr('src');
+        // replace : 교체/치환
+        let num2 = num.replace('_ot','_ov');
+        $(this).find('img').attr('src',num2);
+    })
+
+    $('.main__content--list li').mouseleave(function(){
+        let num = $(this).find('img').attr('src');
+        let num2 = num.replace('_ov','_ot');
+        $(this).find('img').attr('src',num2);
+    })
+})
+
+$(function(){
+    $('.footer__btn--familysite').click(function(){
+        $('.footer__btn--list').stop().slideToggle();
+        $(this).toggleClass('on');
+    })
+})
